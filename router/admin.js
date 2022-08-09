146113228b548743c8272131e35bb45402956505
Aman <amankhanna224466@gmail.com>
2022-08-09 18:30:40 +0530

    admin dashboard

--- router/admin.js
+++ router/admin.js
@@ -4,15 +4,12 @@ const router = Router()
 const ad_productController = require('../controllers/admin/ad_productController')
 const authenticate = require('../middleware/authenticate')
 const admin = require('../middleware/admin')
-const productService = require('../services/productService')
 
-
+router.get('/dashboard', (req, res) => {
+   return res.status(200).render('admin/dashboard')
+})
 router.get('/products/create', /* authenticate, admin, */ ad_productController.renderProductPanel)
 router.post('/products/create', ad_productController.createProduct)
 
-router.get('/test', async (req, res) => {
-   const prods = await productService.getProducts()
-   console.log(prods);
-})
 
 module.exports = router
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: "development",
-    entry: ['./public/js/index.js', './public/js/order.js'],
+    entry: ['./public/js/index.js', './public/js/order.js', './public/js/charts.js'],
     output: {
         filename: 'app.js',
         path: path.resolve(__dirname, './public/js'),
