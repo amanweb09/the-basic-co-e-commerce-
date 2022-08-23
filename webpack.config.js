6c18cb0001ba2f57f6298fd429a3e28d232ae54d
Aman <amankhanna224466@gmail.com>
2022-08-23 08:40:35 +0530

    ui changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: "development",
-    entry: ['./public/js/index.js', './public/js/order.js', './public/js/charts.js'],
+    entry: ['./public/js/index.js', './public/js/order.js', './public/js/email.js'],
     output: {
         filename: 'app.js',
         path: path.resolve(__dirname, './public/js'),
